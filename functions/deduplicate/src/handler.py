79d36a3eaf7962e0269de4325d61a0e15c6982c3
bram <bramelfrink@gmail.com>
2020-02-16 13:31:05 +0100

    Implement first part of deduplication.

--- functions/deduplicate/src/handler.py
+++ functions/deduplicate/src/handler.py
@@ -1,3 +1,10 @@
+"""
+Read input data from S3, remove duplicate rows, remove rows that are already in DynamoDB and write to S3.
+"""
+from helpers.s3 import S3
+from functions.deduplicate.src.deduplicate import deduplicate_input
+
+
 def lambda_handler(event, context):
     """
     The handler is called whenever the Lambda function is invoked.
@@ -7,8 +14,13 @@ def lambda_handler(event, context):
     :return:
     """
     # read from S3
+    s3 = S3(event['bucket'], event['key'], 'deduplicated')
+    df = s3.read()
+
+    # deduplicate input
+    df = deduplicate_input(df, 'click_id')
 
     # deduplicate using DynamoDB
 
     # write to S3
-    pass
+    s3.write(df)
---
+++ helpers/s3.py
@@ -0,0 +1,33 @@
+"""
+Interface to read and write CSV data from and to S3.
+"""
+import pandas as pd
+from pandas import DataFrame
+
+
+class S3:
+    """
+    Facilitates transferring data between step function states.
+    """
+    
+    def __init__(self, bucket: str, key_read: str, folder_write: str):
+        self.bucket = bucket
+        self.key_read = key_read
+        self.folder_write = folder_write
+        
+        self.csv_reader = self.read()
+
+    def read(self) -> DataFrame:
+        """
+        Read CSV file from S3 into a DataFrame
+
+        :return: The CSV file as a DataFrame
+        """
+        df = pd.read_csv(f's3://{self.bucket}/{self.key_read}')
+        return df
+
+    def write(self, df: DataFrame) -> None:
+        """
+        Writes the DataFrame back to S3 as a CSV file.
+        """
+        df.to_csv(f's3://{self.bucket}/{self.folder_write}')
